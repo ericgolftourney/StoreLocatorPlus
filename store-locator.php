4922001d633d29835cb01e4871b0933176c39861
Lance Cleveland <lance@cybersprocket.com>
2010-12-21 17:45:47 -0500

    More code cleanup, formatting.

--- store-locator.php
+++ store-locator.php
@@ -6,7 +6,27 @@ Description: Store Locator Plus is based on the popular Google Maps Store Locato
 Version: 0.1
 Author: Cyber Sprocket Labs
 Author URI: http://www.cybersprocket.com
-*/
+License: GPL3
+
+=====================
+
+	Copyright 2010  Cyber Sprocket Labs (info@cybersprocket.com)
+
+        This program is free software; you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation; either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program; if not, write to the Free Software
+        Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+**/
 
 $sl_version="1.2.39.3";
 $sl_db_version=1.2;
