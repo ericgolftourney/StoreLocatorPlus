61398e682ccfd41da4b622e5fbc27302b0dec462
Lance Cleveland <lance@cybersprocket.com>
2011-04-13 22:54:21 -0400

    load_wp_config had a double-pasted code segment.

--- load_wp_config.php
+++ load_wp_config.php
@@ -32,16 +32,7 @@ if (file_exists($_SERVER['DOCUMENT_ROOT'].'/wp-config.php')) {
 //
 } else if (file_exists($possible_path.'/wp-config.php')) {
     include($possible_path.'/wp-config.php');
-
-// Hopefully we are on the standard relative path
-//
-} else if (file_exists('../../../wp-config.php')) {
-    include('../../../wp-config.php');    
     
-} else if (file_exists('../../../../wp-config.php')) {
-    include('../../../../wp-config.php');    
-}
-
 // Hopefully we are on the standard relative path
 //
 } else if (file_exists('../../../wp-config.php')) {
