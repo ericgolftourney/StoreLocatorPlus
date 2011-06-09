2c790a7701feed2abfaa513911dfd2d89fcf2c5c
Lance Cleveland <lance@cybersprocket.com>
2011-06-09 15:28:05 -0400

    Create execute_and_output_plustemplate()
    closes #5571

--- plus.php
+++ plus.php
@@ -34,6 +34,21 @@ function custom_upload_mimes ( $existing_mimes=array() ) {
 
 } 
 
+/**************************************
+ ** function: execute_and_output_plustemplate()
+ ** 
+ ** Executes the included php (or html) file and prints out the results.
+ ** Makes for easy include templates that depend on processing logic to be
+ ** dumped mid-stream into a WordPress page.  A plugin in a plugin sorta.
+ **
+ ** Parameters:
+ **  $file (string, required) - name of the file in the plugin/templates dir
+ **/
+function execute_and_output_plustemplate($file) {
+    $file = SLPLUS_PLUGINDIR.'/plustemplates/'.$file;
+    print get_string_from_phpexec($file);
+}
+
  
 /**************************************
  ** function: slplus_create_country_pd()
