4d7a088373ac378bde25c8817d46e87a73213811
Lance Cleveland <lance@cybersprocket.com>
2011-06-09 15:30:20 -0400

    Create plustemplates/addlocations_bulkupload.php
    closes #5576

---
+++ plustemplates/addlocations_bulkupload.php
@@ -0,0 +1,14 @@
+<thead>
+    <tr>
+        <th><?php _e("Bulk Upload", $text_domain);?></th>
+    </tr>
+</thead>
+<tr>
+    <td>
+        <div class="add_location_form">
+            <input type="file" name="csvfile" value="" width="150"><br/>
+            <input type='submit' value='<?php _e("Upload Locations", $text_domain);?>' class='button-primary'>
+        </div>
+  </td>
+</tr>
+
