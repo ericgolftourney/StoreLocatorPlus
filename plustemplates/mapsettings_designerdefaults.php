cb9804c8f67b8b0979c447ed95ee81244660a9ac
Lance Cleveland <lance@cybersprocket.com>
2011-06-09 16:43:56 -0400

    Resolve undefined variables errors in plus templates.
    closes #5815

--- plustemplates/mapsettings_designerdefaults.php
+++ plustemplates/mapsettings_designerdefaults.php
@@ -1,12 +1,18 @@
 <div class='form_entry'>
-    <label for='sl_starting_image'>".__("Starting Image",SLPLUS_PREFIX).":</label>
-    <input name='sl_starting_image' value='$sl_starting_image' size='25'><br/>
-    <span class='input_note'>".__("If set, this image will be displayed until a search is performed.",SLPLUS_PREFIX)."</span>
+    <label for='sl_starting_image'><?php _e("Starting Image",SLPLUS_PREFIX); ?>:</label>
+    <input name='sl_starting_image' value='<?php echo get_option('sl_starting_image'); ?>' size='25'><br/>
+    <span class='input_note'><?php _e("If set, this image will be displayed until a search is performed.",SLPLUS_PREFIX); ?></span>
 </div>
     
 <div class='form_entry'>
-    <label for='".SLPLUS_PREFIX."_use_email_form'>".__("Use Email Form",SLPLUS_PREFIX).":</label>
-    <input name='".SLPLUS_PREFIX."_email_form' value='1' type='checkbox' $email_form_checked>
-    <span class='input_note'>".__("Use email form instead of mailto: link when showing email addresses.",SLPLUS_PREFIX)."</span>
+    <label for='<?php echo SLPLUS_PREFIX; ?>_use_email_form'><?php _e("Use Email Form",SLPLUS_PREFIX); ?>:</label>
+    <input name='<?php echo SLPLUS_PREFIX; ?>_email_form' value='1' type='checkbox'
+    <?php
+        if (get_option(SLPLUS_PREFIX.'_email_form') ==1) {
+            echo ' checked';
+        }
+    ?>
+    >
+    <span class='input_note'><?php _e("Use email form instead of mailto: link when showing email addresses.",SLPLUS_PREFIX); ?></span>
 </div>
 
--- plustemplates/mapsettings_searchfeatures.php
+++ plustemplates/mapsettings_searchfeatures.php
@@ -5,27 +5,35 @@
     <input name='sl_use_country_search' 
         value='1' 
         type='checkbox' 
-        <?php echo $country_checked?> 
+        <?php
+        if (get_option('sl_use_country_search') ==1) {
+            echo ' checked';
+        }                
+        ?> 
         >
 </div>
 
 <div class='form_entry'>
-    <label for='<?php echo $prefix?>_show_tag_search'>
+    <label for='<?php echo SLPLUS_PREFIX; ?>_show_tag_search'>
         <?php _e('Tag Input', SLPLUS_PREFIX); ?>:
     </label>
-    <input name='<?php echo $prefix?>_show_tag_search' 
+    <input name='<?php echo SLPLUS_PREFIX; ?>_show_tag_search' 
         value='1' 
         type='checkbox' 
-        <?php echo $show_tag_checked?> 
+        <?php
+        if (get_option(SLPLUS_PREFIX.'_show_tag_search') ==1) {
+            echo ' checked';
+        }                
+        ?> 
         >
 </div>     
 
 <div class='form_entry'>
-    <label for='<?php echo $prefix?>_tag_search_selections'>
+    <label for='<?php echo SLPLUS_PREFIX; ?>_tag_search_selections'>
         <?php _e('Preselected Tag Searches', SLPLUS_PREFIX); ?>:
     </label>
-    <input  name='<?php echo $prefix?>_tag_search_selections' 
-        value='<?php print get_option($prefix.'_tag_search_selections'); ?>' 
+    <input  name='<?php echo SLPLUS_PREFIX; ?>_tag_search_selections' 
+        value='<?php print get_option(SLPLUS_PREFIX.'_tag_search_selections'); ?>' 
         >
     <span class='input_note'>
       <?php 
@@ -35,13 +43,17 @@
 </div>        
 
 <div class='form_entry'>
-    <label for='<?php echo $prefix?>_show_tag_any'>
+    <label for='<?php echo SLPLUS_PREFIX; ?>_show_tag_any'>
         <?php _e('Show "any" on tag pulldown', SLPLUS_PREFIX); ?>:
     </label>
-    <input name='<?php echo $prefix?>_show_tag_any' 
+    <input name='<?php echo SLPLUS_PREFIX; ?>_show_tag_any' 
         value='1' 
         type='checkbox' 
-        <?php echo $show_any_checked?> 
+        <?php
+        if (get_option(SLPLUS_PREFIX.'_show_tag_any')    ==1) {
+            echo ' checked';
+        }                
+        ?> 
         >
     <span class='input_note'>
       <?php 
