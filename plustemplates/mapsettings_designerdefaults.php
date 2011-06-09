4710110ac3b7790b3d32b97c103278b91c445323
Lance Cleveland <lance@cybersprocket.com>
2011-06-09 15:32:45 -0400

    Create mapsettings_designerdefaults.php
    closes #5574

---
+++ plustemplates/mapsettings_designerdefaults.php
@@ -0,0 +1,12 @@
+<div class='form_entry'>
+    <label for='sl_starting_image'>".__("Starting Image",SLPLUS_PREFIX).":</label>
+    <input name='sl_starting_image' value='$sl_starting_image' size='25'><br/>
+    <span class='input_note'>".__("If set, this image will be displayed until a search is performed.",SLPLUS_PREFIX)."</span>
+</div>
+    
+<div class='form_entry'>
+    <label for='".SLPLUS_PREFIX."_use_email_form'>".__("Use Email Form",SLPLUS_PREFIX).":</label>
+    <input name='".SLPLUS_PREFIX."_email_form' value='1' type='checkbox' $email_form_checked>
+    <span class='input_note'>".__("Use email form instead of mailto: link when showing email addresses.",SLPLUS_PREFIX)."</span>
+</div>
+
