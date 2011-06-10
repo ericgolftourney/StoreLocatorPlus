bdec467d94612eb9d4e74f92ff6549c8916affef
Lance Cleveland <lance@cybersprocket.com>
2011-06-10 10:42:06 -0400

    Fix missing $text_domain variable in template.

--- plustemplates/addlocations_bulkupload.php
+++ plustemplates/addlocations_bulkupload.php
@@ -1,13 +1,13 @@
 <thead>
     <tr>
-        <th><?php _e("Bulk Upload", $text_domain);?></th>
+        <th><?php _e("Bulk Upload", SLPLUS_PREFIX);?></th>
     </tr>
 </thead>
 <tr>
     <td>
         <div class="add_location_form">
             <input type="file" name="csvfile" value="" width="150"><br/>
-            <input type='submit' value='<?php _e("Upload Locations", $text_domain);?>' class='button-primary'>
+            <input type='submit' value='<?php _e("Upload Locations", SLPLUS_PREFIX);?>' class='button-primary'>
         </div>
   </td>
 </tr>
