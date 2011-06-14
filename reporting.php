882820c92f293af50f17eea3f6d987e2cce5f1ad
Lance Cleveland <lance@cybersprocket.com>
2011-06-14 15:34:35 -0400

    Start of reporting interface.
    Redmine Ticket: #5845

---
+++ reporting.php
@@ -0,0 +1,13 @@
+<?php
+/****************************************************************************
+ ** file: reporting.php
+ **
+ ** The reporting system
+ ***************************************************************************/
+ global $slplus_plugin;
+?>
+
+<div class="wrap">
+    <?php screen_icon(SLPLUS_PREFIX."_reporting"); ?>
+    <h2><?php _e(sprintf('%s Reporting',$slplus_plugin->name), SLPLUS_PREFIX); ?></h2>
+</div>
