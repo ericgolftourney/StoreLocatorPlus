7ac59d5342a8509af126e41115f5596a7e5e5fff
Lance Cleveland <lance@cybersprocket.com>
2011-06-16 14:45:21 -0400

    Add report summary and report graph showing query/result counts.
    Redmine Ticket: #5840
    Redmine Ticket: #5859
    Redmine Ticket: #5860

--- reporting.php
+++ reporting.php
@@ -4,65 +4,182 @@
  **
  ** The reporting system
  ***************************************************************************/
- global $slplus_plugin;
  
- $slpReportSettings = new wpCSL_settings__slplus(
-        array(
-                'no_license'        => true,
-                'prefix'            => $slplus_plugin->prefix,
-                'url'               => $slplus_plugin->url,
-                'name'              => $slplus_plugin->name . ' Reporting',
-                'plugin_url'        => $slplus_plugin->plugin_url,
-                'render_csl_blocks' => false,
-                'form_action'       => '/wp-admin/admin.php?page='.SLPLUS_PLUGINDIR.'reporting.php',
-            )
-     ); 
- 
-    $slpReportSettings->add_section(
-        array(
-                'name'          => __('Report Parameters',SLPLUS_PREFIX),
-                'description'   => __('Use these settings to select which data to
-                    report on.',SLPLUS_PREFIX),
-                'auto'          => true
-            )
-     );
+global $slplus_plugin, $wpdb;
 
-    // Start of date range to report on
-    // default: 30 days ago
-    //
-    $slpReportStartDate = isset($_POST[SLPLUS_PREFIX.'-start_date']) ?
-        $_POST[SLPLUS_PREFIX.'-start_date'] :
-        date('Y-m-d',time() - (30 * 24 * 60 * 60));
-    $slpReportSettings->add_item(
-        'Report Parameters', 
-        __('Start Date: ',SLPLUS_PREFIX),   
-        'start_date',    
-        'text',
-        null,
-        null,
-        null,
-        $slpReportStartDate
-    ); 
+// Data Settings
+//
+$slpQueryTable     = $wpdb->prefix . 'slp_rep_query';
+$slpResultsTable   = $wpdb->prefix . 'slp_rep_query_results';
  
-    // Start of date range to report on
-    // default: today
-    //
-    $slpReportEndDate = (isset($_POST[SLPLUS_PREFIX.'-end_date'])) ?
-        $_POST[SLPLUS_PREFIX.'-end_date'] :
-        date('Y-m-d',time());
-    $slpReportSettings->add_item(
-        'Report Parameters', 
-        __('End Date: ',SLPLUS_PREFIX),   
-        'end_date',    
-        'text',
-        null,
-        null,
-        null,
-        $slpReportEndDate
-    );     
+// Instantiate the form rendering object
+//
+$slpReportSettings = new wpCSL_settings__slplus(
+    array(
+            'no_license'        => true,
+            'prefix'            => $slplus_plugin->prefix,
+            'url'               => $slplus_plugin->url,
+            'name'              => $slplus_plugin->name . ' Reporting',
+            'plugin_url'        => $slplus_plugin->plugin_url,
+            'render_csl_blocks' => false,
+            'form_action'       => '/wp-admin/admin.php?page='.SLPLUS_PLUGINDIR.'reporting.php',
+        )
+ ); 
+ 
+//------------------------------------
+// Create The Report Parameters Panel
+//  
+$slpReportSettings->add_section(
+    array(
+            'name'          => __('Report Parameters',SLPLUS_PREFIX),
+            'description'   => __('Use these settings to select which data to
+                report on.',SLPLUS_PREFIX),
+            'auto'          => true
+        )
+ );
+
+// Start of date range to report on
+// default: 30 days ago
+//
+$slpReportStartDate = isset($_POST[SLPLUS_PREFIX.'-start_date']) ?
+    $_POST[SLPLUS_PREFIX.'-start_date'] :
+    date('Y-m-d',time() - (30 * 24 * 60 * 60));
+$slpReportSettings->add_item(
+    'Report Parameters', 
+    __('Start Date: ',SLPLUS_PREFIX),   
+    'start_date',    
+    'text',
+    null,
+    null,
+    null,
+    $slpReportStartDate
+); 
+
+// Start of date range to report on
+// default: today
+//
+$slpReportEndDate = (isset($_POST[SLPLUS_PREFIX.'-end_date'])) ?
+    $_POST[SLPLUS_PREFIX.'-end_date'] :
+    date('Y-m-d',time()) . ' 11:59:59';
+$slpReportSettings->add_item(
+    'Report Parameters', 
+    __('End Date: ',SLPLUS_PREFIX),   
+    'end_date',    
+    'text',
+    null,
+    null,
+    null,
+    $slpReportEndDate
+);     
+
+
+//------------------------------------
+// The Summary Graph Panel
+//  
+$slpReportSettings->add_section(
+    array(
+            'name'          => __('Store Locator Plus Usage',SLPLUS_PREFIX),
+            'description'   => '<div id="chart_div"></div>',
+            'auto'          => true
+        )
+ );
+
+// Total searches each day
+//
+// select count(*) as TheCount,DATE(slp_repq_time) as TheDate from wp_slp_rep_query 
+//      where slp_repq_time > '2011-05-17' and 
+//            slp_repq_time <= '2011-06-16 11:59:59' 
+//      group by TheDate;
+$slpReportQuery = sprintf(
+    "SELECT count(*) as TheCount, DATE(slp_repq_time) as TheDate FROM %s " .
+        "WHERE slp_repq_time > '%s' AND " .
+        "      slp_repq_time <= '%s' " .
+        "GROUP BY TheDate",
+    $slpQueryTable,
+    $slpReportStartDate,
+    $slpReportEndDate
+    );
+$slpReportDataset = $wpdb->get_results($slpReportQuery);
+$slpGoogleChartRows = 0;
+$slpGoogleChartData = '';
+foreach ($slpReportDataset as $slpReportDatapoint) {
+    $slpGoogleChartData .= sprintf(
+        "data.setValue(%d, 0, '%s');".
+        "data.setValue(%d, 1, %d);".
+        "data.setValue(%d, 2, %d);",
+        $slpGoogleChartRows,
+        $slpReportDatapoint->TheDate,
+        $slpGoogleChartRows,
+        $slpReportDatapoint->TheCount,
+        $slpGoogleChartRows,        
+        $slpReportDatapoint->TheCount+5
+        );
+    $slpGoogleChartRows++;        
+}    
+
+$slpGoogleChartType = ($slpGoogleChartRows < 2)  ?
+    'ColumnChart' :
+    'LineChart';
+
+
+//------------------------------------
+// The Summary Data Panel
+//
+// Get the total searches in this time span
+//
+// select count(*) from wp_slp_rep_query 
+//      where slp_repq_time > '2011-05-17' and 
+//            slp_repq_time <= '2011-06-16 11:59:59';
+//
+$slpReportQuery = sprintf(
+    "SELECT count(*) FROM %s " .
+        "WHERE slp_repq_time > '%s' AND " .
+        "      slp_repq_time <= '%s' ",
+    $slpQueryTable,
+    $slpReportStartDate,
+    $slpReportEndDate
+    );
+$slpReportDatapoint = $wpdb->get_var($slpReportQuery);
+
+$slpSectionDesc = sprintf(
+    '<div class="reportline total">' .
+        __('Total searches: %s', SLPLUS_PREFIX). 
+    '</div>',
+     $slpReportDatapoint
+    );    
+
+$slpReportSettings->add_section(
+    array(
+            'name'          => __('Summary',SLPLUS_PREFIX),
+            'description'   => $slpSectionDesc,
+            'auto'          => true
+        )
+ );
+
+
 ?>
 
-<?php     
-    $slpReportSettings->render_settings_page();
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript">
+  google.load("visualization", "1", {packages:["corechart"]});
+  google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Date');
+        data.addColumn('number', 'Queries');
+        data.addColumn('number', 'Results');
+        data.addRows(<?php echo $slpGoogleChartRows; ?>);
+        <?php echo $slpGoogleChartData; ?>
+        var chart = new google.visualization.<?php echo $slpGoogleChartType; ?>(document.getElementById('chart_div'));
+        chart.draw(data, {width: 800, height: 400});
+      }
+    </script>
 
-?>
+
+<?php
+
+//------------------------------------
+// Render It 
+//
+$slpReportSettings->render_settings_page();
+
