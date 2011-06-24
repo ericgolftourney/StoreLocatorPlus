d10402a9ab34b62cf2b4ceaee67f3c89f63ed3ff
Lance Cleveland <lance@cybersprocket.com>
2011-06-24 01:24:03 -0400

    Last of search form more info updates.

--- plustemplates/mapsettings_labels.php
+++ plustemplates/mapsettings_labels.php
@@ -1,5 +1,9 @@
 <div class='form_entry'>
     <label for='search_tag_label'><?php _e("Search By Tag Label", SLPLUS_PREFIX); ?>:</label>
     <input name='<?php echo SLPLUS_PREFIX;?>_search_tag_label' value='<?php echo get_option(SLPLUS_PREFIX.'_search_tag_label'); ?>'>
-    <span class='input_note'><?php _e("Label for search form tags field.", SLPLUS_PREFIX); ?></span>                    
+    <?php
+    echo slp_createhelpdiv('sl_website_label',
+        __("Label for search form tags field.", SLPLUS_PREFIX)
+        );
+    ?>                  
 </div>    
