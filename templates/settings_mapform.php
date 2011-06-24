485b361ad721296bae3b2e02f43b284ffda587aa
Lance Cleveland <lance@cybersprocket.com>
2011-06-24 00:39:27 -0400

    Teaking the look.

--- templates/settings_mapform.php
+++ templates/settings_mapform.php
@@ -35,6 +35,58 @@
         </div>
     </div>        
 
+    
+    <div class='section_column'>       
+        <div class='map_designer_settings'>
+            <h2><?php _e('Dimensions', SLPLUS_PREFIX);?></h2>
+            
+            <div class='form_entry'>
+                <label for='zoom_level'><?php _e("Zoom Level", SLPLUS_PREFIX);?>:</label>
+                <?php echo $zoom; ?>
+                <span class='input_note'><?php _e("19=street level, 0=world view. Show locations overrides this setting.",SLPLUS_PREFIX);?></span>
+            </div>
+            
+            <div class='form_entry'>
+                <label for='height'><?php _e("Map Height", SLPLUS_PREFIX);?>:</label>
+                <input name='height' value='<?php echo $height;?>' class='small'>&nbsp;
+                <?php print choose_units($height_units, "height_units"); ?>
+            </div>
+            
+            <div class='form_entry'>
+                <label for='height'><?php _e("Map Width", SLPLUS_PREFIX);?>:</label>
+                <input name='width' value='<?php echo $width;?>'  class='small'>&nbsp;
+                <?php print choose_units($width_units, "width_units"); ?>
+            </div>
+        </div>
+    </div>
+    
+    <div class='section_column'>       
+        <div class='map_designer_settings'>
+            <h2><?php _e('Icons', SLPLUS_PREFIX);?></h2>    
+            <?php echo $icon_notification_msg;?>
+            
+            <div class='form_entry'>
+                <label for='sl_remove_credits'><?php _e('Remove Credits', SLPLUS_PREFIX);?></label>
+                <input name='sl_remove_credits' value='1' type='checkbox' <?php echo $checked3;?> >
+            </div>
+    
+            <div class='form_entry'>
+                <label for='icon'><?php _e('Home Icon', SLPLUS_PREFIX);?></label>
+                <input name='icon' size='45' value='<?php echo $icon;?>' onchange="document.getElementById('prev').src=this.value">
+                    &nbsp;&nbsp;<img id='prev' src='<?php echo $icon;?>' align='top'><br/>
+                <div style='margin-left: 150px;'><?php echo $icon_str;?></div>        
+            </div>
+    
+            <div class='form_entry'>
+                <label for='icon2'><?php _e('Destination Icon', SLPLUS_PREFIX);?></label>
+                <input name='icon2' size='45' value='<?php echo $icon2;?>' onchange="document.getElementById('prev2').src=this.value">
+                    &nbsp;&nbsp;<img id='prev2' src='<?php echo $icon2;?>'align='top'><br/>
+                <div style='margin-left: 150px;'><?php echo $icon2_str;?></div>
+            </div>
+        </div>
+    </div>
+    
+
     <div class='section_column'>   
         <div class='map_interface_settings'> 
             <h2><?php _e('Country', SLPLUS_PREFIX);?></h2>
@@ -62,55 +114,6 @@
                 </select>
             </div>
         </div>
-    </div>
-    
-    <div class='section_column'>       
-        <div class='map_designer_settings'>
-            <h2><?php _e('Dimensions', SLPLUS_PREFIX);?></h2>
-            
-            <div class='form_entry'>
-                <label for='zoom_level'><?php _e("Zoom Level", SLPLUS_PREFIX);?>:</label>
-                <?php echo $zoom; ?>
-                <span class='input_note'><?php _e("19=street level, 0=world view. Show locations overrides this setting.",SLPLUS_PREFIX);?></span>
-            </div>
-            
-            <div class='form_entry'>
-                <label for='height'><?php _e("Map Height", SLPLUS_PREFIX);?>:</label>
-                <input name='height' value='<?php echo $height;?>' class='small'>&nbsp;
-                <?php print choose_units($height_units, "height_units"); ?>
-            </div>
-            
-            <div class='form_entry'>
-                <label for='height'><?php _e("Map Width", SLPLUS_PREFIX);?>:</label>
-                <input name='width' value='<?php echo $width;?>'  class='small'>&nbsp;
-                <?php print choose_units($width_units, "width_units"); ?>
-            </div>
-        </div>
-    </div>
-    <div class='section_column'>       
-        <div class='map_designer_settings'>
-            <h2><?php _e('Design', SLPLUS_PREFIX);?></h2>    
-            <?php echo $icon_notification_msg;?>
-            
-            <div class='form_entry'>
-                <label for='sl_remove_credits'><?php _e('Remove Credits', SLPLUS_PREFIX);?></label>
-                <input name='sl_remove_credits' value='1' type='checkbox' <?php echo $checked3;?> >
-            </div>
-    
-            <div class='form_entry'>
-                <label for='icon'><?php _e('Home Icon', SLPLUS_PREFIX);?></label>
-                <input name='icon' size='45' value='<?php echo $icon;?>' onchange="document.getElementById('prev').src=this.value">
-                    &nbsp;&nbsp;<img id='prev' src='<?php echo $icon;?>' align='top'><br/>
-                <div style='margin-left: 150px;'><?php echo $icon_str;?></div>        
-            </div>
-    
-            <div class='form_entry'>
-                <label for='icon2'><?php _e('Destination Icon', SLPLUS_PREFIX);?></label>
-                <input name='icon2' size='45' value='<?php echo $icon2;?>' onchange="document.getElementById('prev2').src=this.value">
-                    &nbsp;&nbsp;<img id='prev2' src='<?php echo $icon2;?>'align='top'><br/>
-                <div style='margin-left: 150px;'><?php echo $icon2_str;?></div>
-            </div>
-        </div>
-    </div>
+    </div>    
 </div>
 
