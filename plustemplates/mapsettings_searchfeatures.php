1eb6ee71205c052775ece684f4f78c649dddff01
Lance Cleveland <lance@cybersprocket.com>
2011-06-24 01:19:16 -0400

    Mimic standard WPCSL more info buttons on settings form.

--- plustemplates/mapsettings_searchfeatures.php
+++ plustemplates/mapsettings_searchfeatures.php
@@ -35,29 +35,20 @@
     <input  name='<?php echo SLPLUS_PREFIX; ?>_tag_search_selections' 
         value='<?php print get_option(SLPLUS_PREFIX.'_tag_search_selections'); ?>' 
         >
-    <span class='input_note'>
-      <?php 
-        _e("Enter a comma (,) separated list of tags to show in the search pulldown, mark the default selection with parenthesis '( )'. This is a default setting that can be overriden on each page within the shortcode.", SLPLUS_PREFIX); 
-      ?>
-   </span>
+    <?php
+    echo slp_createhelpdiv('tag_search_selections',
+        __("Enter a comma (,) separated list of tags to show in the search pulldown, mark the default selection with parenthesis '( )'. This is a default setting that can be overriden on each page within the shortcode.",SLPLUS_PREFIX)
+        );
+    ?>      
 </div>        
 
 <div class='form_entry'>
-    <label for='<?php echo SLPLUS_PREFIX; ?>_show_tag_any'>
-        <?php _e('Show "any" on tag pulldown', SLPLUS_PREFIX); ?>:
-    </label>
-    <input name='<?php echo SLPLUS_PREFIX; ?>_show_tag_any' 
-        value='1' 
-        type='checkbox' 
-        <?php
-        if (get_option(SLPLUS_PREFIX.'_show_tag_any')    ==1) {
-            echo ' checked';
-        }                
-        ?> 
-        >
-    <span class='input_note'>
-      <?php 
-        _e("If checked the tag pulldown list will have an 'any' option that returns any location.", SLPLUS_PREFIX); 
-      ?>
-   </span>
+    <label for='<?php echo SLPLUS_PREFIX; ?>_show_tag_any'><?php _e('Show "any" on tag pulldown', SLPLUS_PREFIX); ?>:</label>
+    <input name='<?php echo SLPLUS_PREFIX; ?>_show_tag_any' value='1' type='checkbox' <?php if (get_option(SLPLUS_PREFIX.'_show_tag_any')==1) { echo ' checked'; }?> >
+    <?php
+    echo slp_createhelpdiv('show_tag_any',
+        __("If checked the tag pulldown list will have an 'any' option that returns any location.",SLPLUS_PREFIX)
+        );
+    ?> 
 </div>
+
