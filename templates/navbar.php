0c01610ef015a146c5fb44994947a481cee7f8f0
Lance Cleveland <lance@cybersprocket.com>
2011-09-22 18:04:16 -0400

    Navbar "like-a-button" styling.

--- templates/navbar.php
+++ templates/navbar.php
@@ -9,16 +9,16 @@
 ?>
 
 <ul>
-    <li><a href="<?php echo SLPLUS_ADMINPAGE;?>view-locations.php">Locations: Manage</a></li>
-    <li><a href="<?php echo SLPLUS_ADMINPAGE;?>add-locations.php">Locations: Add</a></li>
-    <li><a href="<?php echo SLPLUS_ADMINPAGE;?>map-designer.php">Settings: Map</a></li>    
-    <li><a href="/wp-admin/options-general.php?page=csl-slplus-options">Settings: General</a></li>    
+    <li class='like-a-button'><a href="<?php echo SLPLUS_ADMINPAGE;?>view-locations.php">Locations: Manage</a></li>
+    <li class='like-a-button'><a href="<?php echo SLPLUS_ADMINPAGE;?>add-locations.php">Locations: Add</a></li>
+    <li class='like-a-button'><a href="<?php echo SLPLUS_ADMINPAGE;?>map-designer.php">Settings: Map</a></li>    
+    <li class='like-a-button'><a href="/wp-admin/options-general.php?page=csl-slplus-options">Settings: General</a></li>    
     <?php 
     //--------------------------------
     // Plus Version : Show Reports Tab
     //
     if (!$slplus_plugin->no_license) {
-        print '<li><a href="'.SLPLUS_PLUSPAGE.'reporting.php">Reports</a></li>';
+        print '<li class="like-a-button"><a href="'.SLPLUS_PLUSPAGE.'reporting.php">Reports</a></li>';
     }
     ?>    
 </ul>
