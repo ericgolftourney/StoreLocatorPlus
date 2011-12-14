7553be6817d0fffff1f258037cde9d725246b8a7
Lance Cleveland <lance@cybersprocket.com>
2011-12-14 15:50:45 -0500

    Fix secured installation PHP loader.

--- load_wp_config.php
+++ load_wp_config.php
@@ -1,8 +1,34 @@
 <?php
 
+
 // Find the Wordpress Config File...
 //
+
+// Possible path is the root of this script name, 
+// strip everything from wp-content down
 $possible_path = preg_replace('/\/wp-content\/.*/','',$_SERVER['SCRIPT_FILENAME']);
+
+$secure_path = '';
+if (isset($_SERVER['DOCUMENT_ROOT'])) {
+    $secure_path = dirname($_SERVER['DOCUMENT_ROOT']);
+}
+
+
+/*-----------------DEBUG */
+if (basename($_SERVER['SCRIPT_FILENAME']) == 'load_wp_config.php') {
+    error_reporting(E_ALL);
+    header("Content-type: text/html");
+    print "Possible Path: $possible_path<br/>
+    Secure Path: $secure_path<br/>
+    DOCROOT: " . $_SERVER['DOCUMENT_ROOT'] . "<br/>
+    SCRIPT: " . $_SERVER['SCRIPT_FILENAME'] . "<br/>
+    ";
+    if (file_exists($secure_path.'/wp-config.php')) { print "Found $secure_path/wp-config.php<br/>"; }
+    if (file_exists($secure_path.'/wp-settings.php')) { print "Found $secure_path/wp-settings.php<br/>"; }
+}    
+/*-----------------DEBUG */
+
+
 
 // Document Root Install
 //
@@ -11,11 +37,13 @@ if (isset($_SERVER['DOCUMENT_ROOT']) && file_exists($_SERVER['DOCUMENT_ROOT'].'/
 
 // One Level Up and not part of another install
 //
-} else if (isset($_SERVER['DOCUMENT_ROOT']) && file_exists(dirname($_SERVER['DOCUMENT_ROOT']).'/wp-config.php')
-    && !file_exists(dirname($_SERVER['DOCUMENT_ROOT']).'/wp-settings.php')
+} else if (
+    ($secure_path != '') 
+    && file_exists($secure_path.'/wp-config.php')
+    && !file_exists($secure_path.'/wp-settings.php')
     ) {
-    include(dirname($_SERVER['DOCUMENT_ROOT']).'/wp-config.php');
-
+    define(ABSPATH,$_SERVER['DOCUMENT_ROOT'].'/');
+    include($secure_path.'/wp-config.php');
     
 // Subdomain Install of WordPress
 //
