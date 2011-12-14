d9d1d4e9a40a2568f9249aff464864ebc9a64222
Lance Cleveland <lance@cybersprocket.com>
2011-12-14 16:08:21 -0500

    Refactored the wp_config looker-upper to loop through directories.

--- load_wp_config.php
+++ load_wp_config.php
@@ -1,67 +1,101 @@
 <?php
 
-
-// Find the Wordpress Config File...
-//
-
-// Possible path is the root of this script name, 
-// strip everything from wp-content down
-$possible_path = preg_replace('/\/wp-content\/.*/','',$_SERVER['SCRIPT_FILENAME']);
-
-$secure_path = '';
-if (isset($_SERVER['DOCUMENT_ROOT'])) {
-    $secure_path = dirname($_SERVER['DOCUMENT_ROOT']);
+/*
+ * function: InstallType
+ *
+ * returns the type of WordPress install for the given path:
+ *
+ * 'standard' : the config and settings file are in the given path
+ * 'secured'  : the config file is one directory up, the settings is here
+ * ''         : config and/or settings file is missing
+ * 
+ */
+function InstallType($path) {
+    $upPath = dirname($path);
+    
+    // Check Standard Setup
+    //
+    if (file_exists($path.'/wp-config.php') && file_exists($path.'/wp-settings.php')) {
+        return 'standard';
+    
+    
+    // Check for secured setup
+    //
+    } elseif (
+         file_exists($upPath.'/wp-config.php') && 
+        !file_exists($upPath.'/wp-settings.php') &&
+         file_exists($path.'/wp-settings.php')
+        ) {
+        return 'secured';
+        
+    // Fail
+    //
+    } else {
+        return '';
+    }
 }
 
 
-/*-----------------DEBUG */
-if (basename($_SERVER['SCRIPT_FILENAME']) == 'load_wp_config.php') {
-    error_reporting(E_ALL);
-    header("Content-type: text/html");
-    print "Possible Path: $possible_path<br/>
-    Secure Path: $secure_path<br/>
-    DOCROOT: " . $_SERVER['DOCUMENT_ROOT'] . "<br/>
-    SCRIPT: " . $_SERVER['SCRIPT_FILENAME'] . "<br/>
-    ";
-    if (file_exists($secure_path.'/wp-config.php')) { print "Found $secure_path/wp-config.php<br/>"; }
-    if (file_exists($secure_path.'/wp-settings.php')) { print "Found $secure_path/wp-settings.php<br/>"; }
-}    
-/*-----------------DEBUG */
-
-
-
-// Document Root Install
+// What paths do we want to look at for an install of WordPress?
 //
-if (isset($_SERVER['DOCUMENT_ROOT']) && file_exists($_SERVER['DOCUMENT_ROOT'].'/wp-config.php')) {
-    include($_SERVER['DOCUMENT_ROOT'].'/wp-config.php');
-
-// One Level Up and not part of another install
 //
-} else if (
-    ($secure_path != '') 
-    && file_exists($secure_path.'/wp-config.php')
-    && !file_exists($secure_path.'/wp-settings.php')
-    ) {
-    define(ABSPATH,$_SERVER['DOCUMENT_ROOT'].'/');
-    include($secure_path.'/wp-config.php');
+
+
+// Seed the list with the main directory of our script name
+//
+$possible_path = preg_replace('/\/wp-content\/.*/','',$_SERVER['SCRIPT_FILENAME']);
+$PathsToTry = array($possible_path);
+
+// Check DOCUMENT_ROOT from Apache
+//
+if (isset($_SERVER['DOCUMENT_ROOT'])) { 
+    array_push($PathsToTry,$_SERVER['DOCUMENT_ROOT']); 
+}
+
+// Check SUBDOMAIN_DOCUMENT_ROOT from Apache
+//
+if (isset($_SERVER['SUBDOMAIN_DOCUMENT_ROOT'])) { 
+    array_push($PathsToTry,$_SERVER['SUBDOMAIN_DOCUMENT_ROOT']); 
+}
+
+// Check a few paths up from here
+//
+
+// back up from wordpress/wp-content/plugins/core/js dir
+$thisFileDir = dirname(dirname(dirname(dirname(dirname(__FILE__)))));
+if (file_exists($thisFileDir)) {
+    array_push($PathsToTry,$thisFileDir); 
+}
+// ...and one higher than that
+$thisFileDir = dirname($thisFileDir);
+if (file_exists($thisFileDir)) {
+    array_push($PathsToTry,$thisFileDir); 
+}
+// ...and one higher than that
+$thisFileDir = dirname($thisFileDir);
+if (file_exists($thisFileDir)) {
+    array_push($PathsToTry,$thisFileDir); 
+}
+
+
+// Look for WordPress config info in the given directory list
+// Test both the normal and secured setup
+//
+$installtype = '';
+while ((list(, $thisPath) = each($PathsToTry)) && ($installtype == '')) {
     
-// Subdomain Install of WordPress
-//
-} else if (isset($_SERVER['SUBDOMAIN_DOCUMENT_ROOT']) && file_exists($_SERVER['SUBDOMAIN_DOCUMENT_ROOT'].'/wp-config.php')) {
-    include($_SERVER['SUBDOMAIN_DOCUMENT_ROOT'].'/wp-config.php');
-
-// A sub-directory
-//
-} else if (file_exists($possible_path.'/wp-config.php')) {
-    include($possible_path.'/wp-config.php');
+    // Check for an install at the given path
+    //
+    $installtype = InstallType($thisPath);
     
-// Hopefully we are on the standard relative path
-//
-} else if (file_exists('../../../wp-config.php')) {
-    include('../../../wp-config.php');    
-    
-} else if (file_exists('../../../../wp-config.php')) {
-    include('../../../../wp-config.php');    
+    // If we have a valid install, load the config
+    //
+    if ($installtype == 'standard') {
+        include($thisPath.'/wp-config.php');    
+    } elseif ($installtype == 'secured') {
+        define(ABSPATH,$thisPath.'/');
+        include(dirname($thisPath).'/wp-config.php');
+    }
 }
 
 
