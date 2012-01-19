9f4c976537a61f0507a924c5808d8e11242220c7
Lance Cleveland <lance@cybersprocket.com>
2012-01-19 17:10:14 -0500

    Clean up action bar header.
    closes #7233

---
+++ templates/managelocations_actionbar.php
@@ -0,0 +1,47 @@
+<?php
+/****************************************************************************
+ ** file: core/templates/managelocations_actionbar.php
+ **
+ ** The action bar for the manage locations page.
+ ***************************************************************************/
+ 
+ global $slplus_plugin;
+?>
+<script type="text/javascript">
+function doAction(theAction,thePrompt) {
+    if(confirm(thePrompt)){
+        LF=document.forms['locationForm'];
+        LF.act.value=theAction;
+        LF.submit();
+    }else{
+        return false;
+    }
+}
+</script>
+<div id="action_buttons">
+    <div id="other_actions">
+        <ul>
+        <li class='like-a-button'><a href="#" onclick="doAction('delete','<?php echo __('Delete selected?',SLPLUS_PREFIX);?>')" name="delete_selected"><?php echo __("Delete Selected", SLPLUS_PREFIX); ?></a></li>
+            
+            <?php 
+            //--------------------------------
+            // Plus Version : Show Reports Tab
+            //
+            if ($slplus_plugin->license->packages['Plus Pack']->isenabled) {      
+            }
+            ?>    
+        </ul>
+    </div>
+    <div id="tag_block">
+        <div id="tag_actions">
+            <ul>
+                <li class='like-a-button'><a href="#" name="tag_selected"    onclick="doAction('add_tag','<?php echo __('Tag selected?',SLPLUS_PREFIX);?>');"   ><?php echo __('Tag Selected', SLPLUS_PREFIX);?></a></li>
+                <li class='like-a-button'><a href="#" name="untag_selected"  onclick="doAction('remove_tag','<?php echo __('Remove tag from selected?',SLPLUS_PREFIX);?>');"><?php echo __('Untag Selected', SLPLUS_PREFIX);?></a></li>
+            </ul>
+        </div>
+        <div id="tagentry">
+            <label for="sl_tags"><?php echo __('Tags', SLPLUS_PREFIX); ?></label><input name='sl_tags'>
+        </div>        
+    </div>
+</div>
+
--- view-locations.php
+++ view-locations.php
@@ -50,6 +50,8 @@ if (!$slak) {
 	if (isset($_GET['delete']) && ($_GET['delete']!='')) {
 		$wpdb->query("DELETE FROM ".$wpdb->prefix."store_locator ".
 		    "WHERE sl_id='".$_GET['delete']."'");
+		
+		print "<br/><br/><br/><br/>DELETE<br/>";
 	}
 
     // Edit, any form
@@ -187,15 +189,14 @@ if (!$slak) {
        </div>
         <div style='clear:both;'></div>
     </div>";
+        
+    //-------------------------
+    // Actionbar Section
+    //-------------------------    
+    print '<div id="slplus_actionbar">';
+    print get_string_from_phpexec(SLPLUS_COREDIR.'/templates/managelocations_actionbar.php');
+    print '</div>';   
     
-
-    print "<table width='100%' cellpadding='5px' cellspacing='0' style='border:solid silver 1px' id='rightnow' class='widefat'>
-    <thead><tr>
-    <td style='/*background-color:#000;*/ width:20%'><input class='button' type='button' value='".__("Delete Selected", SLPLUS_PREFIX)."' onclick=\"if(confirm('".__("You sure", SLPLUS_PREFIX)."?')){LF=document.forms['locationForm'];LF.act.value='delete';LF.submit();}else{return false;}\"></td>";
-    print "<td style='/*background-color:#000;*/ width:73%; text-align:right; color:white'>";
-    print "<strong>".__("Tags", SLPLUS_PREFIX)."</strong>&nbsp;<input name='sl_tags'>&nbsp;<input class='button' type='button' value='".__("Tag Selected", SLPLUS_PREFIX)."' onclick=\"LF=document.forms['locationForm'];LF.act.value='add_tag';LF.submit();\">&nbsp;<input class='button' type='button' value='".__("Remove Tag From Selected", SLPLUS_PREFIX)."' onclick=\"if(confirm('".__("You sure", SLPLUS_PREFIX)."?')){LF=document.forms['locationForm'];LF.act.value='remove_tag';LF.submit();}else{return false;}\">";
-    print "</td></tr></thead></table>
-    ";
     set_query_defaults();
 
     //for search links
