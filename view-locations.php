1b1a2f09646e2e8912e3ad7bb7edfbc23777a77c
Lance Cleveland <lance@cybersprocket.com>
2012-01-20 00:10:04 -0500

    Manage locations: tag with none selected generates error.
    closes #7238

--- view-locations.php
+++ view-locations.php
@@ -107,9 +107,38 @@ if (!$slak) {
             include_once(SLPLUS_COREDIR   . 'deleteLocations.php'       );
             
         // Tagging Action
-        }  elseif (eregi("tag", $_REQUEST['act'])) {        
-            include_once(SLPLUS_COREDIR   . 'tagLocations.php'       );
+        }  elseif (eregi("tag", $_REQUEST['act'])) {
             
+            //adding or removing tags for specified a locations
+            if ($_POST) {extract($_POST);}
+            
+            //var_dump($sl_id); exit;
+            if (isset($sl_id)) {
+                if (is_array($sl_id)==1) {
+                    $id_string="";
+                    foreach ($sl_id as $value) {
+                        $id_string.="$value,";
+                    }
+                    $id_string=substr($id_string, 0, strlen($id_string)-1);
+                }
+                else {
+                    $id_string=$sl_id;
+                }
+                if ($act=="add_tag") {
+                    //adding tags
+                    $wpdb->query("UPDATE ".$wpdb->prefix."store_locator SET sl_tags=CONCAT(sl_tags, '".strtolower($sl_tags).", ') WHERE sl_id IN ($id_string)");
+                }
+                elseif ($act=="remove_tag") {
+                    //removing tags
+                    if (empty($sl_tags)) {
+                        //if no tag is specified, all tags will be removed from selected locations
+                        $wpdb->query("UPDATE ".$wpdb->prefix."store_locator SET sl_tags='' WHERE sl_id IN ($id_string)");
+                    }
+                    else {
+                        $wpdb->query("UPDATE ".$wpdb->prefix."store_locator SET sl_tags=REPLACE(sl_tags, '$sl_tags,', '') WHERE sl_id IN ($id_string)");
+                    }
+                }
+            }              
         // Locations Per Page Action
         } elseif ($_REQUEST['act']=="locationsPerPage") {
             //If bulk delete is used
