c0e83bac80ea4315f5004d9d8303c7dcbab7c781
Lance Cleveland <lance@cybersprocket.com>
2012-01-23 12:07:56 -0500

    Remove reference to old themes directory.

--- map-designer.php
+++ map-designer.php
@@ -225,19 +225,7 @@ $map_type["".__("Satellite", SLPLUS_PREFIX).""]="G_SATELLITE_MAP";
 $map_type["".__("Hybrid", SLPLUS_PREFIX).""]="G_HYBRID_MAP";
 $map_type["".__("Physical", SLPLUS_PREFIX).""]="G_PHYSICAL_MAP";
 
-// Custom Themes
-if (is_dir($sl_upload_path."/themes/")) {
-	$theme_dir=opendir($sl_upload_path."/themes/"); 
 
-	while (false !== ($a_theme=readdir($theme_dir))) {
-		if (!ereg("^\.{1,2}$", $a_theme) && !ereg("\.(php|txt|htm(l)?)", $a_theme)) {
-
-			$selected=($a_theme==get_option('sl_map_theme'))? " selected " : "";
-			$theme_str.="<option value='$a_theme' $selected>$a_theme</option>\n";
-		}
-	}
-}
-	
 $zl[]=0;$zl[]=1;$zl[]=2;$zl[]=3;$zl[]=4;$zl[]=5;$zl[]=6;$zl[]=7;$zl[]=8;
 $zl[]=9;$zl[]=10;$zl[]=11;$zl[]=12;$zl[]=13;$zl[]=14;$zl[]=15;$zl[]=16;
 $zl[]=17;$zl[]=18;$zl[]=19;
