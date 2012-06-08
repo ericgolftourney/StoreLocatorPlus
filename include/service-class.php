14b33a8f5474f5e9fbb8e43f309670aed14be214
Rob Landers <robert@cybersprocket.com>
2012-06-08 17:52:16 -0400

    Added sync tags function

--- include/service-class.php
+++ include/service-class.php
@@ -22,6 +22,97 @@ if (! class_exists('slp_service_class')) {
                 }
             }
 
+            function Sync_tags() {
+                global $wpdb;
+                
+                // Initialize the table definitions
+                //
+                $table = $wpdb->prefix."store_locator";
+                $tag_table = $wpdb->prefix."slp_tags";
+                $link_table = $wpdb->prefix."slp_loc_tags";
+
+                //gets all the tags
+                //
+                $query = "SELECT sl_tags, sl_id
+                            FROM $table
+                            WHERE sl_tags <> ''";
+                $results = $wpdb->get_results($query);
+
+                //If there are tags, continue
+                //
+                if ($results) {
+
+                    $tags = array();
+                    $new_tags = array();
+                    $parts = null;
+                    $post_to_tag = array();
+
+                    //get all the tags from the result
+                    //escape them, and add them to our tags list
+                    //
+                    foreach ($results as $tag)
+                    {
+                        $expr = "/,(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))/";
+                        $parts = preg_split($expr, trim($tag->sl_tags));
+                        $parts = preg_replace("/^\"(.*)\"$/","$1",$parts);
+                        $post_to_tag = array();
+                        foreach ($parts as $part) {
+                            
+                            if (trim($part) != '') {
+                                $tags[] = trim($part);
+                                $post_to_tag[] = trim($part);
+                            }
+                        }
+                        var_dump($tags); echo '<br>^^tags<br>';
+
+                        //insert all the tags into the tag table
+                        //
+                        foreach ($post_to_tag as $tagr) {
+                            $col = array(
+                                'tag_name' => $tagr
+                            );
+
+                            // determine the id of an already existing tag
+                            $query = "select tag_id from $tag_table where tag_name = '$tagr';";
+                            $success = $wpdb->get_results($query);
+
+                            if(count($success) != 0) {
+                                $new_tags[$tagr] = $success[0]->tag_id;
+                            }
+                            // it isn't created yet, so create the tag
+                            else {
+                                $success = $wpdb->insert($tag_table, $col);
+                                if (!isset($new_tags[$tagr])) {
+                                    $new_tags[$tagr] = $wpdb->insert_id;
+                                }
+                            }
+
+                            $col = array(
+                                'tag_id' => $new_tags[$tagr],
+                                'sl_id' => $tag->sl_id
+                            );
+                            $success = $wpdb->insert($link_table, $col);                            
+                        }
+                        $query = "select * from $link_table where sl_id = $tag->sl_id and (tag_id != 0 ";
+
+                        foreach ($post_to_tag as $existing_tag) {
+                            $query .= "and tag_id != $new_tags[$existing_tag] ";
+                        }
+
+                        $query .= ");";
+                        $success = $wpdb->get_results($query);
+
+                        // Delete anything that shouldn't be in the table
+                        foreach ($success as $to_delete) {
+                            $query = "delete link.* from wp_slp_loc_tags link where tag_id = $to_delete->tag_id and $to_delete->sl_id;";
+                            $wpdb->query($query);
+                        }
+                    }
+
+                    //die('ready<br>');
+                }
+            }
+
             function SearchTags($tag)
             {
                 global $wpdb;
