8df8154973bed5d1c6003254e681b58f94493ea1
Lance Cleveland <lance@cybersprocket.com>
2012-09-20 10:26:37 -0400

    Re-order the pro pack options for consistency.   Put city/state/country pulldown checks next to each other, for example.

--- core/templates/mapsettings_searchfeatures.php
+++ core/templates/mapsettings_searchfeatures.php
@@ -1,69 +1,54 @@
 <?php
 	global $slplus_plugin;
-	
+
     echo CreateCheckboxDiv(
         '_hide_radius_selections',
         __('Hide radius selection',SLPLUS_PREFIX),
         __('Hides the radius selection from the user, the default radius will be used.', SLPLUS_PREFIX)
         );
-    
+
+    echo CreateCheckboxDiv(
+        '_show_search_by_name',
+        __('Show search by name box', SLPLUS_PREFIX),
+        __('Shows the name search entry box to the user.', SLPLUS_PREFIX)
+        );
+
     echo CreateCheckboxDiv(
         '_hide_address_entry',
         __('Hide address entry box',SLPLUS_PREFIX),
         __('Hides the address entry box from the user.', SLPLUS_PREFIX)
         );
-	
-	if ($slplus_plugin->license->packages['Pro Pack']->isenabled) {
-		echo CreateCheckboxDiv(
-			'_show_search_by_name',
-			__('Show search by name box', SLPLUS_PREFIX),
-			__('Shows the name search entry box to the user.', SLPLUS_PREFIX)
-			);
-	}
+
+    echo CreateCheckboxDiv(
+        '_use_location_sensor',
+        __('Use location sensor', SLPLUS_PREFIX),
+        __('This turns on the location sensor for your customers so they can easily get accurate results')
+    );    
+
+    echo CreateCheckboxDiv(
+            'sl_use_city_search',
+            __('Show City Pulldown',SLPLUS_PREFIX),
+            __('Displays the city pulldown on the search form. It is built from the unique city names in your location list.',SLPLUS_PREFIX),
+            ''
+            );
     
     echo CreateCheckboxDiv(
+        'sl_use_country_search',
+        __('Show Country Pulldown',SLPLUS_PREFIX),
+        __('Displays the country pulldown on the search form. It is built from the unique country names in your location list.',SLPLUS_PREFIX),
+        ''
+        );
+
+    echo CreateCheckboxDiv(
+        'slplus_show_state_pd',
+        __('Show State Pulldown',SLPLUS_PREFIX),
+        __('Displays the state pulldown on the search form. It is built from the unique state names in your location list.',SLPLUS_PREFIX),
+        ''
+        );
+
+
+    echo CreateCheckboxDiv(
         '_disable_search',
-        __('Disable search',SLPLUS_PREFIX),
-        __('This makes the search form non-interactive.  Typically used with the immediately show locations feature with a smaller listing set.', SLPLUS_PREFIX)
-        );            
-
-    //----------------------------------------------------------------------
-    // Pro Pack Enabled
-    //
-    if ($slplus_plugin->license->packages['Pro Pack']->isenabled) {                
-?>
-
-<div class='form_entry'>
-    <label for='slplus_show_state_pd'>
-        <?php _e('Show State Pulldown', SLPLUS_PREFIX); ?>:
-    </label>
-    <input name='slplus_show_state_pd' 
-        value='1' 
-        type='checkbox' 
-        <?php
-        if (get_option('slplus_show_state_pd') ==1) {
-            echo ' checked';
-        }                
-        ?> 
-        >
-</div>
-
-
-<div class='form_entry'>
-    <label for='sl_use_country_search'>
-        <?php _e('Show Country Pulldown', SLPLUS_PREFIX); ?>:
-    </label>
-    <input name='sl_use_country_search' 
-        value='1' 
-        type='checkbox' 
-        <?php
-        if (get_option('sl_use_country_search') ==1) {
-            echo ' checked';
-        }                
-        ?> 
-        >
-</div>
-<?php
-    }    
-?>
-
+        __('Hide Find Locations button',SLPLUS_PREFIX),
+        __('Remove the "Find Locations" button from the search form.', SLPLUS_PREFIX)
+        );
