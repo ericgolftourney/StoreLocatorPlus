56e3c144e883678e79365a047cc7fb9eeced8837
Lance Cleveland <lance@cybersprocket.com>
2012-11-01 17:00:02 -0400

    Remove the missing file from the includes.

--- store-locator-le.php
+++ store-locator-le.php
@@ -84,7 +84,6 @@ if (defined('SLPLUS_PREFIX') === false) {
 global $slplus_plugin;
 include_once(SLPLUS_PLUGINDIR . '/include/config.php'	);
 include_once(SLPLUS_COREDIR   . 'functions.sl.php'	);
-include_once(SLPLUS_COREDIR   . 'csl-ajax-search.php'	);
 
 // General WP Action Interface
 //
