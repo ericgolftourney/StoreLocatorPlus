8d03f9bc7d64b64fa63f652d9322ac200e14e753
Lance Cleveland <lance@cybersprocket.com>
2012-12-18 16:17:26 -0500

    Add div to edit form buttons, minor string edit.

--- include/storelocatorplus-adminui_class.php
+++ include/storelocatorplus-adminui_class.php
@@ -941,7 +941,7 @@ if (! class_exists('SLPlus_AdminUI')) {
                 "<a name='a".$locID."'></a>"                                                    .
                 "<table cellpadding='0' class='slp_locationinfoform_table'>"                           .
                 "<tr><td valign='top'>"                                                         .
-                $slplus_plugin->helper->get_string_from_phpexec(SLPLUS_COREDIR.'/templates/'.'edit_location_address.php')
+                $slplus_plugin->helper->get_string_from_phpexec(SLPLUS_COREDIR.'/templates/edit_location_address.php')
                 ;
 
                 // Store Pages URLs
@@ -967,10 +967,12 @@ if (! class_exists('SLPlus_AdminUI')) {
                     );
                 $slpEditForm .= 
                         ($slplus_plugin->addform? '' : "<span class='slp-edit-location-id'>Location # $locID</span>") .
+                        "<div id='slp_form_buttons'>" .
                         "<input type='submit' value='".($slplus_plugin->addform?__('Add',SLPLUS_PREFIX):__('Update', SLPLUS_PREFIX)).
                             "' alt='$alTitle' title='$alTitle' class='button-primary'>".
                         "<input type='button' class='button' value='".__('Cancel', SLPLUS_PREFIX)."' onclick='location.href=\"".$edCancelURL."\"'>".
-                        "<input type='hidden' name='option_value-$locID' value='".($addform?'':$sl_value['sl_option_value'])."' />" 
+                        "<input type='hidden' name='option_value-$locID' value='".($addform?'':$sl_value['sl_option_value'])."' />"  .
+                        "</div>"
                         ;
 
                 /**
