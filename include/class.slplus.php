e31840332c5465c66d96824714ab83e232d77ecb
Lance Cleveland <lance@charlestonsw.com>
2013-03-07 00:49:12 -0500

    New SLPlus class, on which all other things will hang.
    Extends WPCSL (though this may be flipped later).

---
+++ include/class.slplus.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * The location data interface and management class.
+ *
+ * Make a location an in-memory object and handle persistence via data I/O to the MySQL tables.
+ *
+ * @package StoreLocatorPlus
+ * @subpackage Location
+ * @category DataManagement
+ * @author Lance Cleveland <lance@charlestonsw.com>
+ *
+ * @property int $id
+ * @property string $store
+ * @property string $address
+ * @property string $address2
+ * @property string $city
+ * @property string $state
+ * @property string $zip
+ * @property string $country
+ * @property string $latitude
+ * @property string $longitude
+ * @property string $tags
+ * @property string $description
+ * @property string $email
+ * @property string $url
+ * @property string $hours
+ * @property string $phone
+ * @property string $fax
+ * @property string $image
+ * @property boolean $private
+ * @property string $neat_title
+ * @property int $linked_postid
+ * @property string $pages_url
+ * @property boolean $pages_on
+ * @property string $option_value
+ * @property datetime $lastupdated
+ *
+ */
+class SLPlus_Location {
+
+    private $id;
+    private $store;
+    private $address;
+    private $address2;
+    private $city;
+    private $state;
+    private $zip;
+    private $country;
+    private $latitude;
+    private $longitude;
+    private $tags;
+    private $description;
+    private $email;
+    private $url;
+    private $hours;
+    private $phone;
+    private $fax;
+    private $image;
+    private $private;
+    private $neat_title;
+    private $linked_postid;
+    private $pages_url;
+    private $pages_on;
+    private $option_value;
+    private $lastupdated;
+
+    /**
+     * Fetch a location property from the valid object properties list.
+     *
+     * $currentLocation = new SLPlus_Location();
+     * print $currentLocation->id;
+     * 
+     * @param mixed $property - which property to set.
+     * @return null
+     */
+    public function __get($property) {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        }
+        return null;
+    }
+
+    /**
+     * Set a location property in the valid object properties list to the given value.
+     *
+     * $currentLocation = new SLPlus_Location();
+     * $currentLocation->store = 'My Place';
+     *
+     * @param mixed $property
+     * @param mixed $value
+     * @return \SLPlus_Location
+     */
+    public function __set($property,$value) {
+        if (property_exists($this, $property)) {
+            $this->$property = $value;
+        }
+        return $this;
+    }
+}
