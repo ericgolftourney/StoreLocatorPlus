33935076d25609e4995c1f68963742cbc7578f48
Lance Cleveland <lance@charlestonsw.com>
2013-03-21 16:42:11 -0400

    Start hookup of download to AJAX v. separate program.

--- slp-pro/reporting.js
+++ slp-pro/reporting.js
@@ -9,9 +9,22 @@ jQuery(document).ready(
          var tstts = $("#topsearches_table").tablesorter( {sortList: [[1,1]]} );
          var trtts = $("#topresults_table").tablesorter( {sortList: [[5,1]]} );
 
+         jQuery('#export_results').click(
+            function(e) {
+               var data = {
+                  action: 'slp_download_report_csv',
+                  filename: 'topresults',
+                  query: jQuery("[name=topresults]").val(),
+                  sort: trtts[0].config.sortList.toString(),
+                  all: jQuery("[name=export_all]").is(':checked')
+               };
+               jQuery.post(ajaxurl, data, function(response) { alert('Got server answer:'+response); });
+            }
+         );
+
         // Export Results Button Click
         //
-        jQuery("#export_results").click(
+        jQuery("#export_results_OFFLINE").click(
             function(e) {
                 jQuery('<form action="'+slp_pro.plugin_url+'/slp-pro/downloadcsv.php" method="post">'+
                         '<input type="hidden" name="filename" value="topresults">' +
