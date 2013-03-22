c7a12743ed8bd69898120f9c1862a4900c2b062e
Lance Cleveland <lance@charlestonsw.com>
2013-03-21 23:52:00 -0400

    Get report CSV generation/download going through AJAX.

--- slp-pro/reporting.js
+++ slp-pro/reporting.js
@@ -9,6 +9,8 @@ jQuery(document).ready(
          var tstts = $("#topsearches_table").tablesorter( {sortList: [[1,1]]} );
          var trtts = $("#topresults_table").tablesorter( {sortList: [[5,1]]} );
 
+         // Export Results Click
+         //
          jQuery('#export_results').click(
             function(e) {
                var data = {
@@ -18,37 +20,28 @@ jQuery(document).ready(
                   sort: trtts[0].config.sortList.toString(),
                   all: jQuery("[name=export_all]").is(':checked')
                };
-               jQuery.post(ajaxurl, data, function(response) { alert('Got server answer:'+response); });
+               jQuery('#secretIFrame').attr('src',
+                    ajaxurl + '?' + jQuery.param(data)
+                );
             }
          );
 
-        // Export Results Button Click
-        //
-        jQuery("#export_results_OFFLINE").click(
-            function(e) {
-                jQuery('<form action="'+slp_pro.plugin_url+'/slp-pro/downloadcsv.php" method="post">'+
-                        '<input type="hidden" name="filename" value="topresults">' +
-                        '<input type="hidden" name="query" value="' + jQuery("[name=topresults]").val() + '">' +
-                        '<input type="hidden" name="sort"  value="' + trtts[0].config.sortList.toString() + '">' +
-                        '<input type="hidden" name="all"   value="' + jQuery("[name=export_all]").is(':checked') + '">' +
-                        '</form>'
-                        ).appendTo('body').submit().remove();
-            }
-        );
-
         // Export Searches Button Click
         //
-        jQuery("#export_searches").click(
+         jQuery('#export_searches').click(
             function(e) {
-                jQuery('<form action="'+slp_pro.plugin_url+'/slp-pro/downloadcsv.php" method="post">'+
-                        '<input type="hidden" name="filename" value="topsearches">' +
-                        '<input type="hidden" name="query" value="' + jQuery("[name=topsearches]").val() + '">' +
-                        '<input type="hidden" name="sort"  value="' + tstts[0].config.sortList.toString() + '">' +
-                        '<input type="hidden" name="all"   value="' + jQuery("[name=export_all]").is(':checked') + '">' +
-                        '</form>'
-                        ).appendTo('body').submit().remove();
+               var data = {
+                  action: 'slp_download_report_csv',
+                  filename: 'topsearches',
+                  query: jQuery("[name=topsearches]").val(),
+                  sort: tstts[0].config.sortList.toString(),
+                  all: jQuery("[name=export_all]").is(':checked')
+               };
+               jQuery('#secretIFrame').attr('src',
+                    ajaxurl + '?' + jQuery.param(data)
+                );
             }
-        );
+         );
 
     }
 );
