25dc90b236822ece2223dba9639b6e4ddd6c62e2
Lance Cleveland <lance@charlestonsw.com>
2013-04-02 21:41:48 -0400

    Add the add-on registry.

--- include/class.slplus.php
+++ include/class.slplus.php
@@ -13,6 +13,13 @@
 class SLPlus extends wpCSL_plugin__slplus {
 
     /**
+     * An array of the add-on slugs that are active.
+     * 
+     * @var string[] active add-on slugs 
+     */
+    public $addons = array();
+
+    /**
      * The current location.
      * 
      * @var SLPlus_Location $currentLocation
@@ -38,13 +45,32 @@ class SLPlus extends wpCSL_plugin__slplus {
      *
      * @param mixed[] $params - a named array of the plugin options for wpCSL.
      */
-    function __construct($params) {
+    public function __construct($params) {
         global $wpdb;
         $this->db = $wpdb;
         parent::__construct($params);
         $this->currentLocation = new SLPlus_Location(array('plugin'=>$this));
         $this->themes->css_dir = SLPLUS_PLUGINDIR . 'css/';
         do_action('slp_invocation_complete');
+    }
+
+    /**
+     * Register an add-on pack.
+     * 
+     * @param string $slug
+     */
+    public function register_addon($slug) {
+        list ($t1, $t2) = explode('/', $slug);
+        $this->addons[] = str_replace('.php', '', $t2);
+    }
+
+    /**
+     * Build a query string of the add-on packages.
+     *
+     * @return string
+     */
+    public function create_addon_query() {
+        return http_build_query($this->addons,'addon_');
     }
 
     /**
